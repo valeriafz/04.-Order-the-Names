a9d4d8a728398c55f500b765256a89dcb4802e8c
valeriafz <valeria.frunza33@gmail.com>
2023-11-14 15:45:05 +0200

    initial

---
+++ script.js
@@ -0,0 +1,6 @@
+// Dand click pe butonul add se va adauga valoarea din input in dreptul literei care ii corespunde prima litera din numele introdus,
+// de ex:
+// I. Ion
+// daca sunt mai multe nume cu aceiasi litera atunci se vor desparti prin virgula
+// de ex:
+// I. Ion, Ian
